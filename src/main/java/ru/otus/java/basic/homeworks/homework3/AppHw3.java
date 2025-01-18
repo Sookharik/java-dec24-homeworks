ded96ef3240574c8ce92cbd7088a8fd1c086959c
Aleksandr Panov <san9.aleks@gmail.com>
2025-01-18 18:50:20 +0300

    Сделал задание под звездочкой

--- src/main/java/ru/otus/java/basic/homeworks/homework3/AppHw3.java
+++ src/main/java/ru/otus/java/basic/homeworks/homework3/AppHw3.java
@@ -1,24 +1,37 @@
 package ru.otus.java.basic.homeworks.homework3;
 
+import java.util.Scanner;
+
 public class AppHw3 {
     public static void main(String[] args) {
-        System.out.println("Task 1");
-        greetings();
 
-        System.out.println("Task 2");
-        checkSign(1, 2, -66);
-
-        System.out.println("Task 3");
-        selectColor();
-
-        System.out.println("Task 4");
-        compareNumbers();
-
-        System.out.println("Task 5");
-        addOrSubtractAndPrint(5, 2, true);
+        enterTheNumber();
     }
 
-    //task (1) Реализуйте метод greetings(), который при вызове должен отпечатать в столбец 4 слова: Hello, World, from, Java;
+    // task (*) При запуске приложения, запросите у пользователя число от 1 до 5, и после ввода выполнения метод,
+    // соответствующий указанному номеру со случайным значением аргументов;
+    public static void enterTheNumber() {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            int rnd1 = (int) (Math.random() * 10);
+            int rnd2 = (int) (Math.random() * 10);
+            System.out.println("Введите число от 1 до 5");
+            int difficulty = scanner.nextInt();
+            if (difficulty == 1) {
+                greetings();
+            } else if (difficulty == 2) {
+                checkSign(rnd1, rnd2, -10);
+            } else if (difficulty == 3) {
+                selectColor();
+            } else if (difficulty == 4) {
+                compareNumbers(rnd1, rnd2);
+            } else if (difficulty == 5) {
+                addOrSubtractAndPrint(rnd1, rnd2, true);
+            }
+        }
+    }
+
+    // task (1) Реализуйте метод greetings(), который при вызове должен отпечатать в столбец 4 слова: Hello, World, from, Java;
     public static void greetings() {
         System.out.println("Hello");
         System.out.println("World");
@@ -39,10 +52,14 @@ public class AppHw3 {
     }
 
     // task (3) Реализуйте метод selectColor() в теле которого задайте int переменную data с любым начальным значением.
-    //    Если data меньше 10 включительно, то в консоль должно быть выведено сообщение “Красный”,
-    //    если от 10 до 20 включительно, то “Желтый”, если больше 20 - “Зеленый”;
+    // Если data меньше 10 включительно, то в консоль должно быть выведено сообщение “Красный”,
+    // если от 10 до 20 включительно, то “Желтый”, если больше 20 - “Зеленый”;
     public static void selectColor() {
-        int data = 10;
+        System.out.println("Для отображения Красного цвета введите число от 1 до 10 включительно");
+        System.out.println("Для отображения Желтого цвета введите число от 1 до 20 включительно");
+        System.out.println("Для отображения Зеленого цвета введите число больше 20");
+        Scanner scanner = new Scanner(System.in);
+        int data = scanner.nextInt();
         if (data <= 10) {
             System.out.println("Красный");
         } else if (data > 20) {
@@ -54,9 +71,7 @@ public class AppHw3 {
 
     // task (4) Реализуйте метод compareNumbers(), в теле которого объявите две int переменные a и b с любыми начальными значениями.
     // Если a больше или равно b, то необходимо вывести в консоль сообщение “a >= b”, в противном случае “a < b”;
-    public static void compareNumbers() {
-        int a = 2;
-        int b = 2;
+    public static void compareNumbers(int a, int b) {
         if (a >= b) {
             System.out.println("a >= b");
         } else {
@@ -74,6 +89,3 @@ public class AppHw3 {
         }
     }
 }
-
-
-
