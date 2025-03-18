e26a4eb958f0d8d61e8bf56621ec881b2823a0e9
Aleksandr Panov <san9.aleks@gmail.com>
2025-03-18 16:51:51 +0300

    Готовое домашнее задание

--- src/main/java/ru/otus/java/basic/homeworks/hw15/Main.java
+++ src/main/java/ru/otus/java/basic/homeworks/hw15/Main.java
@@ -12,6 +12,40 @@ package ru.otus.java.basic.homeworks.hw15;
 
 public class Main {
     public static void main(String[] args) {
+        String[][] array = {
+                {"1", "2", "3", "4"},
+                {"5", "6", "7", "8"},
+                {"9", "10", "11", "12"},
+                {"13", "14", "15", "16"}
+        };
 
+        try {
+            int result = processArray(array);
+            System.out.println("Сумма элементов массива: " + result);
+        } catch (AppArraySizeException e) {
+            System.out.println("Ошибка размера массива: " + e.getMessage());
+        } catch (AppArrayDataException e) {
+            System.out.println("Ошибка данных в массиве: " + e.getMessage());
+            System.out.println("Позиция ошибки: строка " + e.getRow() + ", столбец " + e.getCol());
+        }
+    }
+
+    static int processArray(String[][] array) throws AppArraySizeException, AppArrayDataException {
+
+        int sum = 0;
+
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                if (array.length != 4 || array[i].length != 4) {
+                    throw new AppArraySizeException("Массив должен быть размером 4x4.");
+                }
+                try {
+                    sum += Integer.parseInt(array[i][j]);
+                } catch (NumberFormatException e) {
+                    throw new AppArrayDataException("Некорректные данные в ячейке [" + i + "][" + j + "]", i, j);
+                }
+            }
+        }
+        return sum;
     }
 }
